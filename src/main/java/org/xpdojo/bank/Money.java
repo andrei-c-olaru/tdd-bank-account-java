752ce4bd34c9ed79b239cf1223194a8357031daa
andrei <andrei.c.olaru@gmail.com>
2021-01-28 17:13:41 +0200

    Add money class

--- src/main/java/org/xpdojo/bank/Money.java
+++ src/main/java/org/xpdojo/bank/Money.java
@@ -4,6 +4,19 @@ package org.xpdojo.bank;
  * Immutable class to represent Money as a concept.
  * This class should have no accessor methods.
  */
-public class Money {
+public final class Money {
 
+    private int amount = 0;
+
+    public Money(){
+
+    }
+
+    public Money(int amount) {
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
 }
---
+++ src/test/java/org/xpdojo/bank/MoneyTest.java
@@ -0,0 +1,19 @@
+package org.xpdojo.bank;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoneyTest {
+    @Test
+    public void createMoney(){
+        Money money = new Money();
+        assertEquals(money.getAmount(), 0);
+    }
+
+    @Test
+    public void createMoneyNonZero(){
+        Money money = new Money(100);
+        assertEquals(money.getAmount(), 100);
+    }
+}
